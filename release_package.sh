c2869ce5f65727f8073317b18010ff5e3c2a6f38
eugene yeon <eugene@jehmi.com>
2018-12-02 00:29:50 +0900

    Revert "commit"
    This reverts commit 72caae111c9c3e5bbecd41bb084aa266e21bc0e7.

--- release_package.sh
+++
@@ -1,8 +0,0 @@
-#!/usr/bin/env bash
-export ZIP_FILE='lambda.zip'
-export PYTHON_VERSION='python3.6'
-# Zip dependencies from virtualenv, and main.py
-cd venv/lib/$PYTHON_VERSION/site-packages/
-zip -r9 ../../../../$ZIP_FILE *
-cd ../../../../
-zip -g $ZIP_FILE lambda_function.py
